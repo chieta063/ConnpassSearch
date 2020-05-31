da59320f854e2be4bd446617d2f062a3681970bc
HiroakiAbe <chieta063@gmail.com>
2020-05-31 19:03:26 +0900

    first commit

---
+++ ConnpassSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConnpassSearch
+//
+//  Created by 阿部紘明 on 2020/05/31.
+//  Copyright © 2020 阿部紘明. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
